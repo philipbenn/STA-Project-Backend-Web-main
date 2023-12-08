db6b1956427b47eb219bc2076785aa1833e97098
philipbenn <phil658c@stud.kea.dk>
2023-12-08 14:31:02 +0100

    ..

--- STA-Project-Backend-Web-main/src/main/java/dat3/project/course/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package dat3.project.course.repository;
-
-import dat3.project.course.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-}
